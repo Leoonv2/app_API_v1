ffb8c10e6da7bf28d52bdb618f600f9ad44c60c5
Leoonv2 <krjganovleon@gmail.com>
2024-01-11 09:46:51 +0100

    Add .env and prisma/migrations to .gitignore

--- prisma/migrations/20240109134700_/migration.sql
+++
@@ -1,8 +0,0 @@
--- AlterTable
-ALTER TABLE "Category" ALTER COLUMN "updatedAt" DROP NOT NULL;
-
--- AlterTable
-ALTER TABLE "Product" ADD COLUMN     "brand" TEXT,
-ADD COLUMN     "image" TEXT,
-ALTER COLUMN "createdAt" DROP NOT NULL,
-ALTER COLUMN "updatedAt" DROP NOT NULL;
